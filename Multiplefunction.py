d543b85a8deddfb0649cf75f77badd28d5b88d74
Merlin Jiji <merlin.jiji92@gmail.com>
2024-12-19 20:03:30 +0530

    Add files via upload

---
+++ Multiplefunction.py
@@ -0,0 +1,11 @@
+Name=["Sub-field in AI are:","Machine Learning","Neutral Networks","Vision","Robotics","Speech Processing","Natural Language Processing"]
+class Variousfield():
+    def subfields():
+        for data in Name:
+          print(data)
+    def oddeven():
+    num=int(input("Enter a number:"))
+        if(num%2==0):
+          print(num,"is even number")
+        else:
+          print(num,"is odd number")
